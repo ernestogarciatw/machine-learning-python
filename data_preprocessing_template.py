12c0feaf76682636d2778e73ff30d8c7b3bce9d7
Ernesto Garcia <ernesto.garcia@thoughtworks.com>
2020-03-03 14:44:31 -0500

    Missing data example

--- data_preprocessing_template.py
+++ data_preprocessing_template.py
@@ -14,11 +14,3 @@ y = ds.iloc[:, 3].values
 from sklearn.preprocessing import Imputer
 myImputer = Imputer(axis = 0, strategy = 'mean')
 X[:, 1:3] = myImputer.fit(X[:, 1:3]).transform(X[:, 1:3])
-
-
-##2 Categorical Data example
-#from sklearn.preprocessing import LabelEncoder, OneHotEncoder
-#leX = LabelEncoder()
-#X[: , 0] = leX.fit_transform(X[: , 0])
-#oneHotEncoder = OneHotEncoder(categorical_features = [0])
-#X = oneHotEncoder.fit_transform(X).toarray()
