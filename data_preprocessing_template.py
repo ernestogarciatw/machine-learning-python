c3513815d81afd3309922db6bddda5027072ee22
Ernesto Garcia <ernesto.garcia@thoughtworks.com>
2020-03-03 15:00:58 -0500

    Categorical data example

--- data_preprocessing_template.py
+++ data_preprocessing_template.py
@@ -10,7 +10,15 @@ ds = pd.read_csv('Data.csv')
 X = ds.iloc[:, :-1].values
 y = ds.iloc[:, 3].values
 
-##1 Missing data example
+##2 Categorical Data example
 from sklearn.preprocessing import Imputer
-myImputer = Imputer(axis = 0, strategy = 'mean')
-X[:, 1:3] = myImputer.fit(X[:, 1:3]).transform(X[:, 1:3])
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+#First fill out the NaN fields like in the missing data example
+X[:, 1:3] = Imputer(axis = 0, strategy = 'mean').fit(X[:, 1:3]).transform(X[:, 1:3])
+#Then transform data in column 0 to numerical values [0,1,2]
+X[: , 0] = LabelEncoder().fit_transform(X[: , 0])
+#Then transform one column into three with numerical values [0,1]
+myOneHotEncoder = OneHotEncoder(categorical_features = [0])
+X = myOneHotEncoder.fit_transform(X).toarray()
+#Then transform data in y to numerical values [0,1]
+y = LabelEncoder().fit_transform(y)
